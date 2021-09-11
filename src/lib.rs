63075252161a2e109a904e1418ff7af396bde1b4
John Tabash <jrtabash@yahoo.com>
2021-09-11 15:30:04 -0500

    Add lib source file

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+pub mod pcalc_value;
+pub mod pcalc_binary_ops;
+pub mod pcalc_unary_ops;
+pub mod pcalc_environment;
+pub mod pcalc_code;
+pub mod pcalc_lexer;
+pub mod pcalc_parser;
+pub mod pcalc_repl;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-pub mod pcalc_value;
-pub mod pcalc_binary_ops;
-pub mod pcalc_unary_ops;
-pub mod pcalc_environment;
-pub mod pcalc_code;
-pub mod pcalc_lexer;
-pub mod pcalc_parser;
-pub mod pcalc_repl;
-
-use pcalc_repl::REPL;
+use prefix_calculator::pcalc_repl::REPL;
 
 fn main() {
     let mut repl = REPL::new();
